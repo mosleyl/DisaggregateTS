89d46b7ebeece023c8fa555ce56b8543a06749fc
kavehsn <cnobari@gmail.com>
2024-10-07 21:23:37 +0100

    Update kindex.R

--- R/kindex.R
+++ R/kindex.R
@@ -1,22 +1,29 @@
-#' Index of support for LARS algorithm when in high-dimensions 
+#' Index of support for LARS algorithm in high-dimensional settings
 #' 
-#' This function prevents the support of beta becoming greater than \eqn{n_l/2}.
-#' This heuristic approach prevents erratic values of BIC when in high-dimensions. 
+#' This function returns the index where the support of beta coefficients exceeds \eqn{n_l/2}, 
+#' preventing the BIC from becoming erratic in high-dimensional scenarios.
 #' 
-#' @param matrix
-#' @param n_l
+#' @param coef_matrix A matrix of beta coefficients, where rows represent different models.
+#' @param n_l         The length of the low-frequency response series.
+#' @return            The index where the support of beta exceeds \eqn{n_l/2}, or the number of rows of the matrix if no such index is found.
 #' @keywords internal 
 #' @importFrom Rdpack reprompt
 #' @importFrom stats lm rbinom rnorm
 
-k.index <- function(matrix, n_l) {
+k.index <- function(coef_matrix, n_l) {
   
-  count <- apply(matrix, 1, function(x) {sum(x != 0)})
-  if(max(count) > n_l/2) {
-    kindex <- min(which(count > n_l/2))
-  }
-  else {
-    kindex <- nrow(matrix)
+  # Error handling: Ensure that coef_matrix is a matrix and n_l is positive
+  if (!is.matrix(coef_matrix)) stop("'coef_matrix' must be a matrix.")
+  if (!is.numeric(n_l) || n_l <= 0) stop("'n_l' must be a positive numeric value.")
+  
+  # Calculate the count of non-zero coefficients in each row
+  count <- apply(coef_matrix, 1, function(x) {sum(x != 0)})
+  
+  # Determine the index where the count exceeds n_l/2
+  if (max(count) > n_l / 2) {
+    kindex <- min(which(count > n_l / 2))
+  } else {
+    kindex <- nrow(coef_matrix)
   }
   
   return(kindex)
